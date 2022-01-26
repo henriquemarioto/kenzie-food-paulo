7166ce881c14ca583fdefaa12f28fc95c525ea15
henrique.marioto <henrique.marioto@outlook.com>
2022-01-26 09:11:15 -0300

    Token adicionado na api

--- src/js/consumirAPI.js
+++ src/js/consumirAPI.js
@@ -1,6 +1,6 @@
 class ApiRestaurante {
     static async buscaImagens() {
-        return await fetch('https://kenzie-food-api.herokuapp.com/product')
+        return await fetch('https://kenzie-food-api.herokuapp.com/product', {headers: {Authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTEsImlhdCI6MTY0MzExNjkxNiwiZXhwIjoxNjQzOTgwOTE2LCJzdWIiOiJbb2JqZWN0IFVuZGVmaW5lZF0ifQ.KDWjO4BI-xRPROm-D9Inhaix9fJwxlEU5qTyu32kzfU'}})
             .then((response) => response.json())
     }
 }
